4fffe01d7248de26ce777dfa75ef8f24ddc6ab36
Limuthu Nethsuka <limuthunethsuka7@gmail.com>
2025-01-13 08:25:10 +0530

    4.Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,16 +1,17 @@
 import React from 'react';
+
 // Page components
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection.js';
+import FaqSection from '../components/FaqSection.js';
 
-const AboutUs = () => {
-    return(
-        <>
-            <AboutSection />
-            <ServicesSection/>
-        </>
-    )
-    
-}
+
+const AboutUs = () => (
+    <>
+        <AboutSection />
+        <ServicesSection />
+        <FaqSection/>
+    </>
+)
 
 export default AboutUs;
